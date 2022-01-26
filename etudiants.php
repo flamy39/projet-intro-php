2e245c1180a4852dcbc71213c7edffe3ffe96de5
Franck LAMY <franck.lamy39@free.fr>
2022-01-26 11:03:10 +0100

    Prise en compte des icones

--- etudiants.php
+++ etudiants.php
@@ -18,9 +18,10 @@ $etudiants = [
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <title>Tableaux en PHP</title>
 </head>
-<body class="bg-light">
+<body>
 
 <!-- Barre de navigation -->
 <?php
@@ -28,10 +29,10 @@ $etudiants = [
 ?>
 
 <main class="container mt-3">
-    <h1 class="mb-5"><i class="fas fa-user-graduate me-3"></i>Liste des étudiants en BTS SIO</h1>
+    <h1 class="mb-5"><i class="fas fa-user-graduate me-3 text-warning"></i>Liste des étudiants en BTS SIO</h1>
     <div class="d-flex justify-content-between align-items-center">
         <p class="fs-4">Nombre d'étudiants <span class="badge bg-success"><?= sizeof($etudiants) ?> </span></p>
-        <button class="btn btn-success btn-lg"><i class="fas fa-plus-circle me-2"></i>Ajouter un étudiant</button>
+        <button class="btn btn-success btn-lg"> <i class="bi bi-person-plus me-2"></i>Ajouter un étudiant</button>
     </div>
 
     <div class="table-responsive bg-white mt-5 ">
@@ -60,20 +61,9 @@ $etudiants = [
                 <td><?= $etudiant["email"] ?></td>
                 <td><?= $etudiant["promo"] ?></td>
                 <td class="d-flex justify-content-center">
-                    <a href="#" class="btn btn-primary me-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                        </svg>
-                    </a>
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                            data-bs-target="#supprimerUtilisateurModal">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                        </svg>
+                    <a href="#" class="btn btn-primary me-2"><i class="bi bi-pencil-square"></i></a>
+                    <a href="#" class="btn btn-danger me-2"><i class="bi bi-trash"></i></a>
 
-                    </button>
                 </td>
             </tr>
         <?php endforeach; ?>
@@ -111,7 +101,6 @@ $etudiants = [
         integrity="sha512-R/Jhpa6TNKZy3VmvYNBe7nQviLLpNXU64csnTYd6cy64donZkeqfqpXSoJRiZ8M1MCgejev/j5NoTx0rZEy+hg=="
         crossorigin="anonymous"
         referrerpolicy="no-referrer">
-
 </script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Lycée BestStudents</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,13 +7,16 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-home me-1"></i>Accueil</a>
+                    <a class="nav-link" href="#"><i class="fas fa-home me-1" style="color:#8E44AD"></i>Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="etudiants.php"><i class="fas fa-user-graduate me-1"></i>BTS SIO</a>
+                    <?php
+                        $active = $_SERVER["SCRIPT_NAME"]==="/etudiants.php" ? "active" : '';
+                    ?>
+                    <a  href="etudiants.php" class="nav-link <?= $active ?>"><i class="fas fa-user-graduate me-1 text-warning"></i>BTS SIO</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.php"><i class="fas fa-envelope-open-text me-2"></i></i></i>Contact</a>
+                    <a class="nav-link" href="contact.php"><i class="fas fa-envelope-open-text me-2 text-warning"></i></i></i>Contact</a>
                 </li>
             </ul>
         </div>
